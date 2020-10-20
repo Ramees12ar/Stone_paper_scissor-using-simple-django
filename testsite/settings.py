768ac11f40ca35db7225e4c9150f426bac9589b5
Ramees AR <62797124+Ramees12ar@users.noreply.github.com>
2020-10-20 21:12:42 +0530

    Update settings.py

--- testsite/settings.py
+++ testsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_s%wp)**tpicj1a7ezle57mzum+*y0i393!ac@-coekceze257'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stonepaperscisso.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
